47f965ca6809e238c03f04330b7aa70e888ee4c7
Comp191 <cs.comp191@gmail.com>
2018-10-30 13:27:21 +0200

    moved sexpr_eq

--- reader.ml
+++ reader.ml
@@ -23,6 +23,18 @@ type sexpr =
   | Symbol of string
   | Pair of sexpr * sexpr
   | Vector of sexpr list;;
+
+let rec sexpr_eq s1 s2 =
+  match s1, s2 with
+  | Bool(b1), Bool(b2) -> b1 = b2
+  | Nil, Nil -> true
+  | Number(n1), Number(n2) -> n1 = n2
+  | Char(c1), Char(c2) -> c1 = c2
+  | String(s1), String(s2) -> s1 = s2
+  | Symbol(s1), Symbol(s2) -> s1 = s2
+  | Pair(car1, cdr1), Pair(car2, cdr2) -> (sexpr_eq car1 car2) && (sexpr_eq cdr1 cdr2)
+  | Vector(l1), Vector(l2) -> List.for_all2 sexpr_eq l1 l2
+  | _ -> false;;
   
 module Reader: sig
   val read_sexpr : string -> sexpr
@@ -40,18 +52,5 @@ let normalize_scheme_symbol str =
 let read_sexpr string = raise X_not_yet_implemented ;;
 
 let read_sexprs string = raise X_not_yet_implemented;;
-
-let rec sexpr_eq s1 s2 =
-  match s1, s2 with
-  | Bool(b1), Bool(b2) -> b1 = b2
-  | Nil, Nil -> true
-  | Number(n1), Number(n2) -> n1 = n2
-  | Char(c1), Char(c2) -> c1 = c2
-  | String(s1), String(s2) -> s1 = s2
-  | Symbol(s1), Symbol(s2) -> s1 = s2
-  | Pair(car1, cdr1), Pair(car2, cdr2) -> (sexpr_eq car1 car2) && (sexpr_eq cdr1 cdr2)
-  | Vector(l1), Vector(l2) -> List.for_all2 sexpr_eq l1 l2
-  | _ -> false;;
-  
   
 end;; (* struct Reader *)
